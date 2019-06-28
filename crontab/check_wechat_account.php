cc4ca013c9ccd54f7785ab9e842027f5259b934c
何建成 <1484658975@qq.com>
2019-06-28 17:32:12 +0800

    检查微信openid是否是该公众号的openid

---
+++ crontab/check_wechat_account.php
@@ -0,0 +1,97 @@
+<?php
+class Test{
+        private $config=array(
+            'host'=>'127.0.0.1',
+            'user'=>'Jsdgogcuncom',
+            'password'=>'NiNWHh58b8ZC3LdM',
+            'database'=>'Jsdgogcuncom_test',//线下
+//            'user'=>'jsdgogcun',
+//            'password'=>'7XTkmbfrfe',
+//            'database'=>'jsdgogcun',//线上
+            'port'=>'3306',
+        );
+        private  $token;
+        private $conn;
+        private $appid = "wx0fd57bd3a7fc8709";
+//        private $appid = "wx948178e3ae34071a";
+        private $appsecret = "155b63989772784550b867c8a96d23a4";
+//        private $appsecret = "d27ce382ae9c288ada246dffb1c99680";
+        public $table = "ims_weisrc_dish_fans";
+        /**
+         * 测试推送
+         */
+        public function __construct(){
+          $this->conn =  mysqli_connect(
+              $this->config['host'],
+              $this->config['user'],
+              $this->config['password'],
+              $this->config['database'],
+              $this->config['port']
+          );
+            $this->getToken();
+        }
+        public function get_account(){
+            $select = "select * from ".$this->table." where 1";
+            $result = $this->conn->query($select);
+            $sqltoken = "select * from ims_core_cache where key='accesstoken:2'";
+            $this->conn->query($sqltoken);
+            $data =  $this->get_wechat_account();
+            $openid = $data->data->openid;
+            $fan_ids = array();
+            while ( $row= $result->fetch_assoc()){
+                if (!empty($row['from_user'])) {
+                    if (!in_array($row['from_user'],$openid)){
+                        array_push($fan_ids, $row['id']);
+                    }
+                }else{
+                     array_push($fan_ids, $row['id']);
+                }
+            }
+            $ids = implode(',',$fan_ids);
+            file_put_contents('/www/wwwroot/jsd.gogcun.com/ts.log', $ids."\n",8);
+            //   $updatesql = '';
+          //  $this->conn->multi_query($updatesql);
+        }
+
+
+        //获取token 并保存7200秒
+        public function getToken()
+        {
+            $TOKEN_URL="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=". $this->appid."&secret=".$this->appsecret;
+            $data = '';
+            $result = $this->https_post($TOKEN_URL,$data);
+            $json_object = json_decode($result);
+            $this->token= $json_object->access_token;
+            return $this->token;
+        }
+
+        public  function get_wechat_account(){
+
+            $ACC_TOKEN = $this->token;
+            $url = "https://api.weixin.qq.com/cgi-bin/user/get?access_token=".$ACC_TOKEN."&next_openid=";
+            $data = '';
+            $result = $this->https_post($url,$data);
+            $final = json_decode($result);
+            return $final;
+        }
+
+        public function https_post($url,$data)
+        {
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            $result = curl_exec($curl);
+            if (curl_errno($curl)) {
+                return 'Errno'.curl_error($curl);
+            }
+            curl_close($curl);
+            return $result;
+        }
+    }
+    $obj = new Test();
+    $obj->get_account();
+
