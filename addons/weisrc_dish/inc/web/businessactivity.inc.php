03b29f59f57238882a26d9d34fb78d74b2509cbb
何建成 <1484658975@qq.com>
2019-07-16 10:53:55 +0800

    商家端后台限购管理完成

--- addons/weisrc_dish/inc/web/businessactivity.inc.php
+++ addons/weisrc_dish/inc/web/businessactivity.inc.php
@@ -4,10 +4,14 @@ global $_W, $_GPC;
 $weid = $this->_weid;
 $setting = $this->getSetting();
 load()->func('tpl');
-$action = 'activity';
+$action = 'businessactivity';
 $title = $this->actions_titles[$action];
 $storeid = intval($_GPC['storeid']);
-$GLOBALS['frames'] = $this->getMainMenu();
+$this->checkStore($storeid);
+$returnid = $this->checkPermission($storeid);
+$cur_store = $this->getStoreById($storeid);
+$GLOBALS['frames'] = $this->getNaveMenu($storeid,$action);
+
 $operation = !empty($_GPC['op']) ? $_GPC['op'] : 'display';
 if ($operation == 'display') {
     $condition = " a.weid=:weid";
@@ -28,14 +32,17 @@ if ($operation == 'display') {
     if ($_GPC['goods_name']){
         $condition .= " AND a.goodsid = {$_GPC['goods_name']}";
     }
-    $condition .= " AND a.storeid = 0 AND a.deleted = 0";
+    if (!empty($storeid)){
+        $condition .= " AND a.storeid = {$storeid}";
+    }
+    $condition .= " AND a.deleted = 0";
     $pindex = max(1, intval($_GPC['page']));
     $psize = 8;
 
     $goods_name_sql = "select distinct a.goodsid,b.title from
             ".tablename($this->table_goods_activity)."as a left join
             " .tablename('weisrc_dish_goods')." as  b on  b.id=a.goodsid  where  a.weid=:weid and a.storeid=:storeid ORDER BY updatetime DESC,a.id DESC ";
-    $goods_name = pdo_fetchall($goods_name_sql,  array(':weid' => $weid, ':storeid' => 0));
+    $goods_name = pdo_fetchall($goods_name_sql,  array(':weid' => $weid, ':storeid' => $storeid));
 
 
     $start = ($pindex - 1) * $psize;
@@ -57,19 +64,16 @@ if ($operation == 'display') {
             " .tablename('weisrc_dish_goods')." as  b on  b.id=a.goodsid  where  a.id = {$id}";
         $goods = pdo_fetch($sql);
         $goodsid = $goods["goodsid"];
-        $storeid = $goods["storeid"];
         $marketprice = $goods['marketprice'];
         $activityprice = $goods['activityprice'];
         $counts = $goods['counts'];
-        $goodslist = pdo_fetchall("SELECT * FROM " . tablename($this->table_goods) . " WHERE deleted=0 AND status=1 AND storeid={$storeid}");
     }
-    $where_store = "WHERE weid = {$weid} AND deleted=0";
-    $storeslist = pdo_fetchall("SELECT * FROM " . tablename($this->table_stores) . " {$where_store} order by displayorder desc,id desc");
+    $goodslist = pdo_fetchall("SELECT * FROM " . tablename($this->table_goods) . " WHERE deleted=0 AND status=1 AND storeid={$storeid}");
     if (checksubmit('submit')) {
         $data = array(
             'goodsid' => intval($_GPC['goods']),
             'weid' => intval($_W['uniacid']),
-            'storeid' => 0,
+            'storeid' => $storeid,
             'counts' => intval($_GPC['counts']),
             'activityprice' => floatval($_GPC['activityprice']),
             'startdate' => strtotime($_GPC['datelimit']['start']),
@@ -96,45 +100,32 @@ if ($operation == 'display') {
             message("限购价只能小于等于商品原价!");
         }
         //控制一个商品只能在一个时间段参加活动，不能重叠
-        $sql_activity = "select * from " . tablename($this->table_goods_activity) . "as c where c.deleted = 0 and c.goodsid = {$data['goodsid']} and c.weid=:weid and  not (c.`startdate`>{$data["enddate"]} or c.`enddate`<{$data["startdate"]})";
-        $activity  = pdo_fetchall($sql_activity,  array(':weid' => $weid));
+        $sql_businessactivity = "select * from " . tablename($this->table_goods_activity) . "as c where c.deleted = 0 and c.goodsid = {$data['goodsid']} and c.weid=:weid and  not (c.`startdate`>{$data["enddate"]} or c.`enddate`<{$data["startdate"]})";
+        $businessactivity  = pdo_fetchall($sql_businessactivity,  array(':weid' => $weid));
         if (!empty($id)) {
-            if (count($activity) > 1){
+            if (count($businessactivity) > 1){
                 message('该商品在该时间段内已有限购活动', '', 'error');
             }
             pdo_update($this->table_goods_activity, $data, array('id' => $id, 'weid' => $_W['uniacid']));
         } else {
-            if (count($activity) > 0){
+            if (count($businessactivity) > 0){
                 message('该商品在该时间段内已有限购活动', '', 'error');
             }
             pdo_insert($this->table_goods_activity, $data);
         }
-        message('操作成功!', $this->createWebUrl('activity', array('op' => 'display')));
+        message('操作成功!', $this->createWebUrl('businessactivity', array('op' => 'display', 'storeid' => $storeid)));
     }
 }elseif ($operation == 'delete'){
     $id = intval($_GPC['id']);
     $item = pdo_fetch("SELECT id FROM " . tablename($this->table_goods_activity) . " WHERE id = :id AND weid=:weid", array(':id' => $id, ':weid' => $weid));
     if (empty($item)) {
-        message('抱歉，不存在或是已经被删除！', $this->createWebUrl('activity', array('op' => 'display', 'storeid' => $storeid)), 'error');
+        message('抱歉，不存在或是已经被删除！', $this->createWebUrl('businessactivity', array('op' => 'display', 'storeid' => $storeid)), 'error');
     }
     pdo_query("UPDATE " . tablename($this->table_goods_activity) . " SET deleted = 1 WHERE id=:id", array( ':id' => $id));
-    message('删除成功！', $this->createWebUrl('activity', array('op' => 'display')), 'success');
-}elseif($operation == "getgoods"){
-    global $_W, $_GPC;
-    $storeid = $_GPC["storeid"];
-    $weid = $this->_weid;
-    if ($storeid !=''){
-        $where = "AND storeid  = {$storeid} AND deleted=0 AND status=1";
-        $goodslist = pdo_fetchall("SELECT title,id FROM " . tablename($this->table_goods) . " WHERE weid=:weid {$where} ORDER BY displayorder DESC,id DESC", array(':weid' => $weid), 'id');
-    }
-    $data["msg"] = "提示";
-    $data["data"] = $goodslist;
-    $result = array('data' =>$data, 'status' => 1);
-    echo json_encode($result);
-    exit();
+    message('删除成功！', $this->createWebUrl('businessactivity', array('op' => 'display', 'storeid' => $storeid)), 'success');
 }elseif($operation == "getgoodsprice"){
     global $_W, $_GPC;
-    $goodsid = $_GPC["goodsid"];
+    $goodsid = intval($_GPC["goodsid"]);
     $weid = $this->_weid;
     $where = "AND id  = {$goodsid} AND deleted=0 AND status=1";
     $goodslist = pdo_fetchall("SELECT title,id,marketprice FROM " . tablename($this->table_goods) . " WHERE weid=:weid {$where} ORDER BY displayorder DESC,id DESC", array(':weid' => $weid));
@@ -146,4 +137,4 @@ if ($operation == 'display') {
     echo json_encode($result);
     exit();
 }
-include $this->template('web/activity');
+include $this->template('web/businessactivity');
